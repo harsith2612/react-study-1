fe39e27cbc5b8beb5eb0edf795700f5cfd131996
harshith.nayak <harshith.nayak@99games.in>
2024-07-31 15:59:59 +0530

    prj1 done

--- project1bg/src/App.jsx
+++ project1bg/src/App.jsx
@@ -4,30 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("black");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-screen" style={{backgroundColor:color}}>
+        
+
+        <div className="fixed flex flex-wrap justify-center  bottom-12 inset-x-0 px-2">
+        <div className="bg-white flex flex-wrap justify-center gap-3 shadow-xl px-3 py-2 rounded-lg">
+          <button className='rounded-full px-4  py-1 text-white shadow-2xl' style={{backgroundColor:"red"}} onClick={()=>setColor('red')}>
+            red
+          </button>
+
+          <button className='rounded-full px-4  py-1 text-white shadow-2xl' style={{backgroundColor:"limegreen"}} onClick={()=>setColor('limegreen')}>
+            yellow
+          </button>
+
+          <button className='rounded-full px-4  py-1 text-white shadow-2xl' style={{backgroundColor:"orange"}} onClick={()=>setColor('orange')}>
+            orange
+          </button>
+
+          <button className='rounded-full px-4  py-1  shadow-2xl' style={{backgroundColor:"lavender"}} onClick={()=>setColor('lavender')}>
+            lavender
+          </button>
+        </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
